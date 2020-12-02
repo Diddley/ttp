09635e3d5de6e6cd6d0ec78c211b74d08bd3205f
Steve Solomon <steve@thesolomons.me>
2020-12-02 14:38:08 +0000

    datatables formatted and add entity button

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['steve@delphicapital.co.uk']
-    POSTS_PER_PAGE = 8
+    POSTS_PER_PAGE = 100
     COMMENTS_PER_PAGE = 3
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'uploads')
